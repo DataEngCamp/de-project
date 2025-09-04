746c0de998ed041fe2ab475f3e1917a5bb9d6339
enzoapu <enzoapu@gmail.com>
2025-09-04 18:45:34 +0800

    create simplfy celery crawler tasks

--- data_ingestion/worker.py
+++ data_ingestion/worker.py
@@ -8,21 +8,14 @@ from data_ingestion.config import (
     WORKER_PASSWORD,
 )
 
-# print(
-#     f"""
-#     RABBITMQ_HOST: {RABBITMQ_HOST}
-#     RABBITMQ_PORT: {RABBITMQ_PORT}
-#     WORKER_USERNAME: {WORKER_USERNAME}
-#     WORKER_PASSWORD: {WORKER_PASSWORD}
-# """
-# )
-
 app = Celery(
     main="worker",
+    # 載入以下路徑模組註冊的 Celery 任務
     include=[
         "data_ingestion.tasks",
         "data_ingestion.tasks_crawler_hahow_course",
         "data_ingestion.tasks_crawler_hahow_article",
+        "data_ingestion.tasks_crawler_hahow",
     ],
     # 指定 broker 為 rabbitmq
     # pyamqp://worker:worker@127.0.0.1:5672/
@@ -30,3 +23,4 @@ app = Celery(
 )
 
 print(f"CPU 核心數: {multiprocessing.cpu_count()}")
+print("🚀 Celery Worker 已啟動，等待接收任務...")
